79874f4accebfc34ed8c8eb7cfbebc07d32c6bca
Andriy Gridnevskiy <andregridn@gmail.com>
2023-10-30 10:35:47 +0200

    HW2 — Цикли, умовні конструкції, примітивні типи даних в деталях

---
+++ 02_homework/01_task/script.js
@@ -0,0 +1,8 @@
+'use strict'
+
+for (let i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) { console.log("ЛолКек") }
+    else if (i % 3 === 0) { console.log("Лол") }
+    else if (i % 5 === 0) { console.log("Кек") }
+    else { console.log(i) }
+}
