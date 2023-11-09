b4af59e5f52fcd5b27bc87e69fec5472f19ac6f4
Andriy Gridnevskiy <andregridn@gmail.com>
2023-11-09 18:42:00 +0200

    Додав значення по замовчуванню і запуск функції без аргументів для демонстрації, що все працює :)

--- 05_homework/01_task/script.js
+++ 05_homework/01_task/script.js
@@ -1,8 +1,9 @@
 'use strict';
 
-const durationBetweenDates = (begin, end, format) => {
+const durationBetweenDates = (begin = '22 Apr 1995', end = '09 Nov 2023', format = 'days') => {
     const beginDate = new Date(begin);
     const endDate = new Date(end);
+    console.log(format);
     let duration;
     if (beginDate < endDate) {
         duration = endDate.getTime() - beginDate.getTime();
@@ -11,10 +12,11 @@ const durationBetweenDates = (begin, end, format) => {
     if (format === 'seconds') {
         console.log(duration / 1000 + ' seconds')
     } else if (format === 'days') {
-        console.log(duration / 1000 / 60 / 60 / 24 + ' days')
+        console.log((duration / 1000 / 60 / 60 / 24).toFixed(0) + ' days')
     }
 
 };
 
+durationBetweenDates();
 durationBetweenDates('02 Aug 1985', '03 Aug 1985', 'seconds');  // поверне '86400 seconds'
 durationBetweenDates('31 Jan 2022', '03 Feb 2021', 'days');  // поверне '362 days'
